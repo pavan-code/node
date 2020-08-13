33250657fa39786e9d02aa2f4731f11133441564
pavan-code <pavankumaranguluri1@gmail.com>
2020-08-14 00:50:59 +0530

    basic auth

--- app.js
+++ app.js
@@ -3,18 +3,18 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-const mongoose = require('mongoose');
+// var auth = require('basic-auth');
 
 var Dishes = require('./models/dishes')
 var Promotions = require('./models/promotions');
 var Leaders = require('./models/leaders');
 
+const mongoose = require('mongoose');
 const url = 'mongodb://localhost:27017/conFusion';
 const connect = mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex : true});
 
 connect.then(db => {
-  console.log("connected to db successfully");
+  console.log("connected to database successfully");
 }, err => { console.log(err);})
 
 var indexRouter = require('./routes/index');
@@ -33,6 +33,37 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// authentication  = = = = == = = = = = = = = = = = = = = = = 
+function auth(req, res, next) {
+  console.log(req.headers);
+
+  var authHeader = req.headers.authorization;
+  if(!authHeader) {
+    var err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    next(err);
+  }
+  else {
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':');   
+
+    var username = auth[0];
+    var password = auth[1];
+
+    if( username === 'admin' && password === '1234') {
+      next();
+    }
+    else {
+      var err = new Error('Invalid username or password');
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      next(err);
+    }
+  }
+}
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
